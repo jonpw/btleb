be9b95fe00f20213988c8f655a887a80ee550149
jon <dooberx@gmail.com>
2020-08-30 22:51:51 +1000

    Update btleb.py

--- btleb.py
+++ btleb.py
@@ -1,8 +1,13 @@
 import gatt, binascii, struct
+#from influxwriter import InfluxWriter
+#from mqttwriter import MqttWriter
 
 manager = gatt.DeviceManager(adapter_name='hci0')
 
-class AnyDevice(gatt.Device):
+#influxwriter = InfluxWriter(hostname, database=ASSET_DB_NAME, user=INFLUX_USER, password=INFLUX_PASS)
+#mqttwriter = MqttWriter(hostname, topic_prefix=MQTT_BASE_TOPIC, user=MQTT_USER, password=MQTT_PASS)
+
+class XiaoMiTHDevice(gatt.Device):
     def services_resolved(self):
         super().services_resolved()
 
@@ -14,12 +19,12 @@ class AnyDevice(gatt.Device):
             if c.uuid == 'ebe0ccc1-7a0a-4b0c-8a1a-6ff2997da3a6')
         temperature_humidity_characteristic.enable_notifications()
 
-        #temperature_humidity_characteristic.read_value()
-
     def characteristic_value_updated(self, characteristic, value):
-        #print("Temperature and humidity:", binascii.hexlify(value))
         tempraw, humidityraw, uk1, uk2 = struct.unpack('<hbcc', value)
-        print(f"Temperature and humidity: {str(tempraw/100)}, {str(humidityraw)}")
+        temperature = tempraw/100
+        humidity = humidityraw
+        print(f"Temperature and humidity: {str(temperature)}, {str(humidity)}")
+        #influxwriter({"device": self.mac_address, "data": {"temperature": temperature, "humidity": humidity}})
 
     def characteristic_enable_notification_succeeded(self, characteristic):
         print(f"Enabled notification succeeded on {characteristic}")
@@ -27,8 +32,11 @@ class AnyDevice(gatt.Device):
     def characteristic_enable_notification_failed(self, characteristic):
         print(f"Enabled notification failed on {characteristic}")
 
-
-device = AnyDevice(mac_address='A4:C1:38:7F:7B:F1', manager=manager)
-device.connect()
+btmacs = ('A4:C1:38:7F:7B:F1', 'A4:C1:38:A9:98:38')
+devices = []
+for btmac in btmacs:
+    device = XiaoMiTHDevice(mac_address=btmac, manager=manager)
+    device.connect()
+    devices.append(device)
 
 manager.run()
